fd57017f0da545e2920aa5d1ce4e77f3f8b53916
José Ángel <joseangelmt@outlook.com>
2021-04-09 06:55:05 +0200

    Refactorization

--- Digi21.DigiNG.IO.Bin/Bin.cs
+++ Digi21.DigiNG.IO.Bin/Bin.cs
@@ -13,34 +13,34 @@ namespace Digi21.DigiNG.IO.Bin
         public event ResultsAddedEventHandler ResultsAdded;
         public event TaskAddedEventHandler TaskAdded;
 
-        public Bin(string path, int decimals, Point3D globalOrigin) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DelegateGetWkt GetWkt) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, bool readOnly) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, bool readOnly) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DelegateGetWkt GetWkt, bool readOnly) { throw null; }
-        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, bool readOnly) { throw null; }
-        public ReadOnlyComplex Add(Complex complex) { throw null; }
-        public void Add(Entity entity) { throw null; }
-        public ReadOnlyLine Add(Line line) { throw null; }
-        public ReadOnlyPoint Add(Point point) { throw null; }
-        public ReadOnlyPolygon Add(Polygon polygon) { throw null; }
-        public ReadOnlyText Add(Text text) { throw null; }
-        public void Add(IEnumerable<Entity> entities) { throw null; }
-        public void Add(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes) { throw null; }
-        public void Delete(Entity entity) { throw null; }
-        public void Delete(IEnumerable<Entity> entities) { throw null; }
-        public void Dispose() { throw null; }
-        public IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity) { throw null; }
-        public IEnumerator<Entity> GetEnumerator() { throw null; }
-        IEnumerator IEnumerable.GetEnumerator() { throw null; }
-        public Point3D GlobalOrigin { get; }
-        public int Decimals { get; }
-        public string Wkt { get; }
-        public IDictionary<string, int> DatabaseTables { get; }
-        public bool CanWrite { get; }
-        public bool CanRead { get; }
-        public string Path { get; }
+        public Bin(string path, int decimals, Point3D globalOrigin) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DelegateGetWkt GetWkt) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, bool readOnly) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, bool readOnly) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DelegateGetWkt GetWkt, bool readOnly) => throw null;
+        public Bin(string path, int decimals, Point3D globalOrigin, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, bool readOnly) => throw null;
+        public ReadOnlyComplex Add(Complex complex) => throw null;
+        public void Add(Entity entity) => throw null;
+        public ReadOnlyLine Add(Line line) => throw null;
+        public ReadOnlyPoint Add(Point point) => throw null;
+        public ReadOnlyPolygon Add(Polygon polygon) => throw null;
+        public ReadOnlyText Add(Text text) => throw null;
+        public void Add(IEnumerable<Entity> entities) => throw null;
+        public void Add(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes) => throw null;
+        public void Delete(Entity entity) => throw null;
+        public void Delete(IEnumerable<Entity> entities) => throw null;
+        public void Dispose() => throw null;
+        public IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity) => throw null;
+        public IEnumerator<Entity> GetEnumerator() => throw null;
+        IEnumerator IEnumerable.GetEnumerator() => throw null;
+        public Point3D GlobalOrigin => throw null;
+        public int Decimals => throw null;
+        public string Wkt => throw null;
+        public IDictionary<string, int> DatabaseTables => throw null;
+        public bool CanWrite => throw null;
+        public bool CanRead => throw null;
+        public string Path => throw null;
     }
 }
