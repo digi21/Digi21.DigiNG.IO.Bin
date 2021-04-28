138ef7875722fabcc396dcccc057000f3fce0277
José Ángel <joseangelmt@outlook.com>
2021-04-28 15:52:55 +0200

    Added property ConnectionString. digi21/TareasDigi3D#355

--- Digi21.DigiNG.IO.Bin/Bin.cs
+++ Digi21.DigiNG.IO.Bin/Bin.cs
@@ -43,5 +43,6 @@ namespace Digi21.DigiNG.IO.Bin
         public bool CanWrite => throw null;
         public bool CanRead => throw null;
         public string Path => throw null;
+        public string ConnectionString => throw null;
     }
 }
