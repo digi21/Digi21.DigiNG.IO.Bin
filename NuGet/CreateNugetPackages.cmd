c91dbabb2eafa027a39368b8d1641a4d8c3e6f63
José Ángel <joseangelmt@outlook.com>
2021-10-23 08:45:28 +0200

    Updated timestamp server

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe pack Digi21.DigiNG.IO.Bin.nuspec
-nuget.exe sign Digi21.DigiNG.IO.Bin.21.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://tsa.starfieldtech.com
+nuget.exe sign Digi21.DigiNG.IO.Bin.21.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
