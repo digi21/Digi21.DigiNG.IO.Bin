c4defd7eaef3b959715731f1cb2ba6c02cc4f3f6
José Ángel <joseangelmt@outlook.com>
2021-12-10 13:29:32 +0100

    IReadOnlyDrawingFile implements IWindow3D digi21/TareasDigi3D#432

--- Digi21.DigiNG.IO.Bin/Bin.cs
+++ Digi21.DigiNG.IO.Bin/Bin.cs
@@ -42,5 +42,27 @@ namespace Digi21.DigiNG.IO.Bin
         public bool CanRead => throw null;
         public string Path => throw null;
         public string ConnectionString => throw null;
+
+        #region IWindow3D
+        public Point3D W => throw null;
+        public Point3D SW => throw null;
+        public Point3D S => throw null;
+        public Point3D SE => throw null;
+        public Point3D E => throw null;
+        public Point3D NE => throw null;
+        public Point3D N => throw null;
+        public Point3D NW => throw null;
+        public Point3D Center => throw null;
+        public double Depth => throw null;
+        public double Height => throw null;
+        public double Width => throw null;
+        public bool Valid => throw null;
+        public double Zmax => throw null;
+        public double Ymax => throw null;
+        public double Xmax => throw null;
+        public double Zmin => throw null;
+        public double Ymin => throw null;
+        public double? Xmin => throw null;
+        #endregion
     }
 }
